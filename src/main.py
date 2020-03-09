7db5f756651aee85891f5fe3d1ae812399c5df53
Marta Banel <01130552@pw.edu.pl>
2020-03-09 21:11:34 +0100

    [WIP] Add general outline

--- src/main.py
+++ src/main.py
@@ -1,16 +1,80 @@
+import copy
 import json
 
 import config
 import visualize
 import taskfilter
 
+class Operation:
+    pass#todo
 
-def process_task(task):
-    pass
+
+class Information:
+    pass#todo
+
+
+class Results:
+    pass#todo
+
+
+def calculate(input, info, operations):
+    input.get_objects()
+    results = []
+    paths = info.get_paths()
+    for operations in paths:
+        output = copy.deepcopy(input)
+        for operation in operations:
+            output = operation.exec(output)
+        results.append(output)
+    return results
+
+
+def process_input_output(info, input, output):
+    input.get_objects() #set objects property
+    output.get_objects()
+    num_divisions = len(input.objects)
+    for i in range(num_divisions):
+        for op1 in operations:
+            for op2 in operations:
+                if op2.exec(op1.exec(input)).equals(output) #todo: equals
+                    info.add_path([op1, op2])
+
+
+def process_task(id, task, operations, results):
+    info = new Information()
+
+    num_train = len(task['train'])
+    for i in range(num_train):
+        process_input_output(task['train'][i]['input'], task['train'][i]['output'], info, operations)
+
+    num_test = len(task['test'])
+    if num_test == 1:
+        result = calculate(task['test'][0]['input'], info)
+        results.add(id, result)
+        # task['test'][0]['output'] = result
+    else:
+        for i in range(num_test):
+            result = calculate(task['test'][i]['input'], info)
+            results.add(id, result)
+            # task['test'][0]['output'] = result
+
+
+# def flattener(pred):
+#     str_pred = str([row for row in pred])
+#     str_pred = str_pred.replace(', ', '')
+#     str_pred = str_pred.replace('[[', '|')
+#     str_pred = str_pred.replace('][', '|')
+#     str_pred = str_pred.replace(']]', '|')
+#     return str_pred
 
 
 if __name__ == "__main__":
+    results = new Results()
+    operations = []#todo
     for task in taskfilter.filter_tasks_by_number_of_colors(config.training_tasks,0,2,True):
-        # process_task(task)
+
+        print(task)
+        # process_task(config.training_tasks[i], task, operations, results)
         # print(task)
-        visualize.plot_task(task) 
+        visualize.plot_task(task)
+    results.save('submission.csv')
