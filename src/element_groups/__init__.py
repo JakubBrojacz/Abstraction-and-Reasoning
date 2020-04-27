f5cdd80aa5d0b792d21de400232a1b709a8824e1
Marta Banel <01130552@pw.edu.pl>
2020-04-27 02:40:29 +0200

    Add Extract operation and ContainsUniqueColor group

--- src/element_groups/__init__.py
+++ src/element_groups/__init__.py
@@ -1,9 +1,11 @@
 from .all import All
 from .biggest import Biggest
 from .at_edge import AtEdge
+from .contains_unique_color import ContainsUniqueColor
 
 ELEMENT_GROUPS = [
     All,
     Biggest,
-    AtEdge
+    AtEdge,
+    ContainsUniqueColor
 ]
--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -1,8 +1,10 @@
+from .extract import Extract
 from .move import Move
 from .symmetry import Symmetry
 from .filling_elements import FillElements
 
 OPERATIONS = [
+    Extract,
     Move,
     Symmetry,
     FillElements
