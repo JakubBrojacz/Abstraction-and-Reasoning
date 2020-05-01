76b6afe3301af5caf99660cef0344dde3a86b079
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2020-05-01 16:37:18 +0200

    add horizontally divided option

--- src/operations/intersect_two_parts_of_board.py
+++ src/operations/intersect_two_parts_of_board.py
@@ -6,26 +6,31 @@ from config import number_of_colors
 class InterSectTwoPartsOfBoard(Operation):
     @classmethod
     def run_operation(cls, board: Board, elements, args):
-        if board.width % 2 == 0:
-            return None
-
         half_board_width = board.width/2;
+        half_board_height = board.height/2;
 
-        intersect_color = board.matrix[0][half_board_width]
+        if is_vertically_divided(board):
+            matrix = [
+                [background_color for col in range(half_board_width)]
+                for row in range(board.height)]
+            new_board = board.Board(matrix)    
 
-        for i in range(board.height):
-            if  board.matrix[i][half_board_width] != intersect_color:
-                return None
+            for i in range(board.height):
+                for j in range(half_board_width):
+                    if board.matrix[i][j] == board.matrix[i][half_board_width + j + 1]:
+                        new_board.matrix[i][j] = 1
+        elif is_horizontally_divided(board):
+            matrix = [
+                [background_color for col in range(board.width)]
+                for row in range(half_board_height)]
+            new_board = board.Board(matrix)    
 
-        matrix = [
-            [background_color for col in range(board.width/2)]
-            for row in range(board.height)]
-        new_board = board.Board(matrix)    
-
-        for i in range(board.height):
-            for j in range(board.width/2):
-                if board.matrix[i][j] == board.matrix[i][second_half_board_width + j + 1]:
-                    new_board.matrix[i][j] = 1
+            for i in range(half_board_height):
+                for j in range(board.width):
+                    if board.matrix[i][j] == board.matrix[half_board_height + i + 1][j]:
+                        new_board.matrix[i][j] = 1
+        else:
+            return None
 
         return new_board
 
@@ -34,3 +39,32 @@ class InterSectTwoPartsOfBoard(Operation):
         for i in range(number_of_colors):
             yield i
 
+
+def is_horizontally_divided(board):
+    if board.height % 2 == 0:
+        return False
+
+    half_board_height = board.height/2;
+
+    intersect_color = board.matrix[half_board_height][0]
+
+    for i in range(board.width):
+        if  board.matrix[half_board_height][i] != intersect_color:
+            return False
+
+    return True
+
+
+def is_vertically_divided(board):
+    if board.width % 2 == 0:
+        return False
+
+    half_board_width = board.width/2;
+
+    intersect_color = board.matrix[0][half_board_width]
+
+    for i in range(board.height):
+        if  board.matrix[i][half_board_width] != intersect_color:
+            return False
+
+    return True
