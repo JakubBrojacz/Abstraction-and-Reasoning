6ba642d51130ec2693ff875d7ff954a638b5851d
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2020-05-04 19:58:42 +0200

    fixes

--- src/operations/intersect_two_parts_of_board.py
+++ src/operations/intersect_two_parts_of_board.py
@@ -1,38 +1,65 @@
 from operations.operation import Operation
 from board import Board
 from element import Element
-from config import number_of_colors
+from config import number_of_colors, background_color
 
 class InterSectTwoPartsOfBoard(Operation):
     @classmethod
     def run_operation(cls, board: Board, elements, args):
-        half_board_width = board.width/2;
-        half_board_height = board.height/2;
+        half_board_width = int(board.width/2);
+        half_board_height = int(board.height/2);
 
         if is_vertically_divided(board):
             matrix = [
-                [background_color for col in range(half_board_width)]
+                [0 for col in range(half_board_width)]
                 for row in range(board.height)]
-            new_board = board.Board(matrix)    
 
             for i in range(board.height):
                 for j in range(half_board_width):
                     if board.matrix[i][j] == board.matrix[i][half_board_width + j + 1]:
-                        new_board.matrix[i][j] = 1
+                        matrix[i][j] = 1
+
+            board.matrix = [
+                [background_color for col in range(half_board_width)]
+                for row in range(board.height)]
+            board.elements = []
+            board.element_group = []
+            board.element_group_counter = []
+
+            for i in range(board.height):
+                for j in range(half_board_width):
+                    if matrix[i][j] == 1:
+                        el = Element([[args for x in range(1)] for y in range(1)], (i, j), args)
+                        board.elements.append(el)
+                        board.element_group_counter.append(el)
+
         elif is_horizontally_divided(board):
             matrix = [
-                [background_color for col in range(board.width)]
+                [0 for col in range(board.width)]
                 for row in range(half_board_height)]
-            new_board = board.Board(matrix)    
 
             for i in range(half_board_height):
                 for j in range(board.width):
                     if board.matrix[i][j] == board.matrix[half_board_height + i + 1][j]:
-                        new_board.matrix[i][j] = 1
+                        matrix[i][j] = 1
+
+            board.matrix = [
+                [background_color for col in range(board.width)]
+                for row in range(half_board_height)]
+            board.elements = []
+            board.element_group = []
+            board.element_group_counter = []
+
+            for i in range(half_board_height):
+                for j in range(board.width):
+                    if matrix[i][j] == 1:
+                        el = Element([[args for x in range(1)] for y in range(1)], (i, j), args)
+                        board.elements.append(el)
+                        board.element_group_counter.append(el)
         else:
             return None
 
-        return new_board
+        return board
 
     @classmethod
     def gen_args(cls, board, elements):
@@ -44,7 +71,7 @@ def is_horizontally_divided(board):
     if board.height % 2 == 0:
         return False
 
-    half_board_height = board.height/2;
+    half_board_height = int(board.height/2);
 
     intersect_color = board.matrix[half_board_height][0]
 
@@ -59,7 +86,7 @@ def is_vertically_divided(board):
     if board.width % 2 == 0:
         return False
 
-    half_board_width = board.width/2;
+    half_board_width = int(board.width/2);
 
     intersect_color = board.matrix[0][half_board_width]
 
