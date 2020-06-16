cb0b6bddb76ecd4752bf0a65121af4921e1e214b
Jakub Brojacz <kubabr@gmail.com>
2020-06-16 02:08:30 +0200

    Remove evaluation & test from config

--- src/config.py
+++ src/config.py
@@ -3,11 +3,8 @@ from pathlib import Path
 data_path = Path('data/')
 training_path = data_path / 'training_sample'
 # training_path = data_path / 'training'
-evaluation_path = data_path / 'evaluation'
-test_path = data_path / 'test'
 
 training_tasks = sorted(training_path.iterdir())
-evaluation_tasks = sorted(evaluation_path.iterdir())
 
 processing_strategy = "ONE_BY_ONE"
 max_result_boards = 3
